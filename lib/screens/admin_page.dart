02863e8e2078210dde57eba7d6637d102d37e10c
Zayniddin <tuit0778@gmail.com>
2022-10-20 22:28:59 +0500

    delete post metodlari ishladi

--- lib/screens/admin_page.dart
+++ lib/screens/admin_page.dart
@@ -126,4 +126,6 @@ class _AdminPageState extends State<AdminPage> {
           });
     }
   }
+
+  
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -68,34 +68,130 @@ class _HomePageState extends State<HomePage> {
       ),
       itemCount: medicineList.length,
       itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (_) =>
-                    InfoPage(medicineId: medicineList[index].id ?? 0),
-              ),
-            );
-          },
-          child: Container(
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.all(12),
-            child: Column(
-              children: [
-                Expanded(
-                  child: Image.network(
-                    medicineList[index].imageUrl,
-                    width: 120,
-                  ),
+        return Dismissible(
+            behavior: HitTestBehavior.deferToChild,
+            key: Key(index.toString()),
+            direction: DismissDirection.startToEnd,
+            background: Container(
+              color: Colors.red,
+              child: Center(
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                  size: 32,
                 ),
-                SizedBox(height: 12),
-                Text(medicineList[index].name)
-              ],
+              ),
             ),
+            onDismissed: (direction) {
+              if (direction == DismissDirection.startToEnd) {
+                showDeleteDialog(medicineList[index].id);
+              }
+            },
+            child: medicineItem(medicineList, index));
+      },
+    );
+  }
+
+  Widget medicineItem(medicineList, index) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => InfoPage(medicineId: medicineList[index].id ?? 0),
           ),
         );
       },
+      child: Container(
+        padding: EdgeInsets.all(12),
+        margin: EdgeInsets.all(12),
+        child: Column(
+          children: [
+            Expanded(
+              child: Image.network(
+                medicineList[index].imageUrl,
+                width: 120,
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(medicineList[index].name)
+          ],
+        ),
+      ),
     );
+  }
+
+  void showDeleteDialog(num? id) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Delete ??"),
+            content: Text("Rostdan ham o'chirmoqchimisiz ?"),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      Navigator.pop(context);
+                    });
+                  },
+                  child: Text("No")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    callDelete(id);
+                  },
+                  child: Text("Yes")),
+            ],
+          );
+        });
+  }
+
+  void callDelete(num? id) async {
+    String url =
+        "https://pharmacy-app-management.herokuapp.com/api/drugs/${id}";
+
+    var response = await http.delete(Uri.parse(url));
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('SUCCESS'),
+              content: Text(response.body.toString()),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      Navigator.pop(context);
+                    });
+                  },
+                  child: Text("OK"),
+                )
+              ],
+            );
+          });
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('ERROR'),
+              content: Text(response.body.toString()),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("OK"),
+                )
+              ],
+            );
+          });
+    }
   }
 }
